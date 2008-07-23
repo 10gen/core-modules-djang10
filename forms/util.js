dfa02d14d2cbef55b2ae5cd9a10448e32fbe5981
Mike Dirolf <mike@10gen.com>
2008-07-23 13:44:58 -0400

    Added a simple python formatter and attribute flattener to util. Unit tests for util.

--- forms/util.js
+++ forms/util.js
@@ -16,10 +16,92 @@
 
 util = {};
 
-var ErrorDict 
-    util.ErrorDict 
-    = function() {
+var simplePythonFormat = util.simplePythonFormat = function (msg, vals) {
+    if (typeof(msg) != 'string') {
+        throw new FormatterError('Message must be of type string.');
+    }
     
+    var re = /\%(?:\((\w+)\))?([\w\%])/;
+    
+    var getReplacement = function (v, code) {
+        if (!v)
+            return null;
+
+        switch (code) {
+            case "s":
+                return v.toString();
+            case "r":
+                return tojson(v).toString();
+            case "i":
+            case "d":
+                return parseInt(v).toString();
+            case "f":
+                return Number(v).toString();
+            case "%":
+                return "%"
+            default:
+                throw new FormatterError('Unexpected conversion type: ' + code)
+        }
+    };
+    
+    var arg_index = 1;
+    var named = false;
+    
+    while ((match = re.exec(msg)) != null) {
+        if (match[2] == "%")
+            arg_index --;
+
+        if (arg_index >= arguments.length)
+            throw new FormatterError('Not enough arguments to replace all conversion specifiers.');
+        
+        // named replacement    
+        if(match[1]) {
+            var repl = getReplacement(vals[match[1]], match[2]);
+            if (!repl)
+                throw new FormatterError('Could not find mapping for key: ' + match[1]);
+            named = true
+        }
+        // anonymous replacement
+        else {
+            repl = getReplacement(arguments[arg_index], match[2]);
+            arg_index++;
+        }
+        
+        msg = msg.replace(re, repl);
+    }
+    if (arg_index < arguments.length && !named)
+        throw new FormatterError('Too many arguments.');
+    
+    return msg;
+}
+
+// Note: the django version gives a leading space, this doesn't.
+var flatatt = util.flatatt = function(dict) {
+    var buffer = "";
+    var isFirst = true;
+    
+    for(var key in dict) {
+        if(!isFirst)
+            buffer += ' ';
+        buffer += (key + '=' + '"' + content.HTML.escape(dict[key]) + '"');
+        
+        isFirst = false;
+    }
+    return buffer;
+}
+
+var NotImplementedError = util.NotImplementedError = function(msg) {
+    this.message = msg;
 };
 
-ErrorDict.prototype.
+var FormatterError = util.FormatterError = function(msg) {
+    this.message = msg;
+}
+
+var ValidationError = util.ValidationError = function(msg) {
+    this.message = msg;
+};
+
+ValidationError.NON_FIELD_ERRORS = '__all__';
+
+return util;
---
+++ test/test_forms_util.js
@@ -0,0 +1,45 @@
+/**
+*      Copyright (C) 2008 10gen Inc.
+*  
+*    Licensed under the Apache License, Version 2.0 (the "License");
+*    you may not use this file except in compliance with the License.
+*    You may obtain a copy of the License at
+*  
+*       http://www.apache.org/licenses/LICENSE-2.0
+*  
+*    Unless required by applicable law or agreed to in writing, software
+*    distributed under the License is distributed on an "AS IS" BASIS,
+*    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*    See the License for the specific language governing permissions and
+*    limitations under the License.
+*/
+
+core.modules.djang10.forms.util();
+core.modules.djang10.test.test();
+
+/**
+ * simplePythonFormat tests
+ */
+test.assertThrows(new util.FormatterError('Message must be of type string.'), null, util.simplePythonFormat, null);
+assert(util.simplePythonFormat('') == '');
+assert(util.simplePythonFormat('hello') == 'hello');
+test.assertThrows(new util.FormatterError('Message must be of type string.'), null, util.simplePythonFormat, 1);
+test.assertThrows(new util.FormatterError('Not enough arguments to replace all conversion specifiers.'), null, util.simplePythonFormat, 'hello %d');
+test.assertThrows(new util.FormatterError('Not enough arguments to replace all conversion specifiers.'), null, util.simplePythonFormat, 'hello %d %s', "100 s");
+test.assertThrows(new util.FormatterError('Could not find mapping for key: dog'), null, util.simplePythonFormat, '%(cat)s and %(dog)s', {cat: "tom", mouse: "jerry"});
+test.assertThrows(new util.FormatterError('Unexpected conversion type: c'), null, util.simplePythonFormat, 'hello %c', 'd');
+assert(util.simplePythonFormat('%s %s', 'hello', 'world') == 'hello world');
+assert(util.simplePythonFormat('%(verb)s %(adverb)s', {verb: 'run', adverb: 'quickly'}) == 'run quickly');
+assert(util.simplePythonFormat('what about a number like %d?', 5) == 'what about a number like 5?');
+assert(util.simplePythonFormat('%d %i %s %r %f %%', 5, '24.00', 10, {hello: 'world'}, 5.01) == "5 24 10 { \"hello\" : \"world\"  }\n 5.01 %");
+assert(util.simplePythonFormat('%(adverb)s %(verb)s', {test: 'test', verb: 'run', adverb: 'quickly'}, 4) == 'quickly run');
+/*
+    TODO better test coverage for simplePythonFormat
+*/
+
+/**
+ * flatatt tests
+ */
+assert(util.flatatt({}) == "");
+assert(util.flatatt({'id': "header"}) == 'id="header"');
+assert(util.flatatt({"class": "news", "title": "Read this"}) == 'class="news" title="Read this"');
