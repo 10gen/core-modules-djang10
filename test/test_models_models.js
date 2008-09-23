54fcd5994fe360400012c9a6fcc7a5591ac2dd8f
Mike Dirolf <mike@10gen.com>
2008-09-23 11:17:02 -0400

    new format for dbcollection toString(): update tests

--- test/test_models_models.js
+++ test/test_models_models.js
@@ -1,12 +1,12 @@
 /**
 *      Copyright (C) 2008 10gen Inc.
-*  
+*
 *    Licensed under the Apache License, Version 2.0 (the "License");
 *    you may not use this file except in compliance with the License.
 *    You may obtain a copy of the License at
-*  
+*
 *       http://www.apache.org/licenses/LICENSE-2.0
-*  
+*
 *    Unless required by applicable law or agreed to in writing, software
 *    distributed under the License is distributed on an "AS IS" BASIS,
 *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -59,7 +59,7 @@ assert(Article.objects.find().count() === 2);
 assert(Article.objects.find()[0].some_field === 10);
 assert(Article.objects.find()[1].some_field === 20);
 
-assert(Article.__collection.toString() === "{DBCollection:testapp.Article}");
+assert(Article.__collection.toString() === "testapp.Article");
 
 // Try using a different collection name
 var Article = models.new_model({
@@ -95,7 +95,7 @@ assert(Article.objects.find().count() === 2);
 assert(Article.objects.find()[0].some_field === 10);
 assert(Article.objects.find()[1].some_field === 20);
 
-assert(Article.__collection.toString() === "{DBCollection:testapp.Custom}");
+assert(Article.__collection.toString() === "testapp.Custom");
 
 // Test having a property named id
 var Article = models.new_model({
