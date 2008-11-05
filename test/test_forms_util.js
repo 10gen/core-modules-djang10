2659cdda064bff5bf33bb0833ace26a1c947f51c
Mike Dirolf <mike@10gen.com>
2008-11-05 14:48:06 -0500

    update output to match changes to ed

--- test/test_forms_util.js
+++ test/test_forms_util.js
@@ -1,12 +1,12 @@
 /**
 *      Copyright (C) 2008 10gen Inc.
-*  
+*
 *    Licensed under the Apache License, Version 2.0 (the "License");
 *    you may not use this file except in compliance with the License.
 *    You may obtain a copy of the License at
-*  
+*
 *       http://www.apache.org/licenses/LICENSE-2.0
-*  
+*
 *    Unless required by applicable law or agreed to in writing, software
 *    distributed under the License is distributed on an "AS IS" BASIS,
 *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -162,4 +162,4 @@ var VeryBadError = function() {};
 VeryBadError.prototype.toString = function() {return "A very bad error.";};
 
 assert("" + new util.ValidationError(new VeryBadError()).message == '<ul class="errorlist"><li>A very bad error.</li></ul>');
-assert("" + new util.ValidationError({hello: " world"}).message == '<ul class="errorlist"><li>Object</li></ul>');
+assert("" + new util.ValidationError({hello: " world"}).message == '<ul class="errorlist"><li>[object Object]</li></ul>');
